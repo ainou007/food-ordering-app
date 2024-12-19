b766e1964c96b8e127aff7f78a09d2c2dcb70c44
Abdel-ain <ainou.abdelmounim@gmail.com>
2024-12-19 18:54:28 +0100

    Setup the database

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        //  Accepter sulement les protocoles https
+        protocol: "https",
+
+        // Accepter n'importe quelle hostname
+        hostname: "**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
